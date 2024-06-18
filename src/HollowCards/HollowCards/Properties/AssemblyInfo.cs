92ef289ec17cb261f4d623ced78533470ce62472
Mike Holloway <40776983+mholloway24@users.noreply.github.com>
2024-06-17 22:22:40 -0700

    Upgrade to .Net 8

--- src/HollowCards/HollowCards/Properties/AssemblyInfo.cs
+++ src/HollowCards/HollowCards/Properties/AssemblyInfo.cs
@@ -1,15 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("HollowCards")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("An Electrorent Company")]
-[assembly: AssemblyProduct("HollowCards")]
-[assembly: AssemblyCopyright("Copyright © An Electrorent Company 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a35713d0-c485-459f-9f39-19dda323c4a4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
